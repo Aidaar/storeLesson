238d62a994e433a41f2bae934d578954eefea415
Aidaar <aidar21mamatov@gmail.com>
2023-05-27 11:58:11 +0600

    add props on cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cart from './components/Cart/Cart'
+
+import img1 from './assets/cart/img1.jpeg'
+import img2 from './assets/cart/img2.jpeg'
+import img3 from './assets/cart/img3.jpeg'
+import img4 from './assets/cart/img4.jpeg'
+import img5 from './assets/cart/img5.jpeg'
+import img6 from './assets/cart/img6.jpeg'
+import img7 from './assets/cart/img7.jpeg'
+import img8 from './assets/cart/img8.jpeg'
+
+const products = [
+  {
+    image: img1,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img2,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img3,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img4,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img5,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img6,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img7,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  },
+  {
+    image: img8,
+    title: 'Kostenlose Rücksendungen innerhalb von 90 Tagen',
+    desc: 'Temu kompensiert die Kohlenstoffemissionen jeder Lieferung',
+    price: '64,79'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     {
+      products.map(product => (
+        <Cart 
+        image={product.image}
+        title={product.title}
+        desc={product.desc}
+        price={product.price}
+        />
+      ))
+     }
     </div>
   );
 }
